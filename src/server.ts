d1112e849527680b2d2f9f6a394af3916d69739e
Matheus Amorim <mtsgamorim@gmail.com>
2022-09-19 02:15:16 -0300

    changing console.log message when server is online

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT: Number = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server rodando na porta: ${PORT}`);
+  console.log(`Server funcionando na porta: ${PORT}`);
 });
